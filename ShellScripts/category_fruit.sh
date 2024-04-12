3c6c2755745e05184fccd99be30cabce0e4dc95b
twming <tanwoeiming@gmail.com>
2024-04-12 15:47:00 +0800

    Update category_fruit.sh

--- ShellScripts/category_fruit.sh
+++ ShellScripts/category_fruit.sh
@@ -2,7 +2,7 @@
 
 i=1
 echo "Now is `date` : " > /home/linux/LinuxShellScript/ShellScripts/category_fruit_result.txt
-for myfruit in $(<fruit_data)
+for myfruit in $(</home/linux/LinuxShellScript/ShellScripts/fruit_data)
 do
     echo "$i. $myfruit" >> /home/linux/LinuxShellScript/ShellScripts/category_fruit_result.txt
     ((i++))
