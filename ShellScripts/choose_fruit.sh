c6ee651d56b03d4e3d213e30a252bb3af56dabaf
twming <tanwoeiming@gmail.com>
2024-07-14 18:08:08 +0800

    update

---
+++ ShellScripts/choose_fruit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+i=1
+echo "Now is `date` : " > ~/choose_5_fruit_result.txt
+
+HOUR=`date +%k`
+echo "Now is `date` : " >> ~/LinuxShellScript/linkdata/current/$HOUR/record.txt
+
+echo "Research shows that eating fruits is good for health." >> ~/choose_5_fruit_result.txt
+echo "The fruits of the day are:" >> ~/choose_5_fruit_result.txt
+
+echo "Research shows that eating fruits is good for health." >> ~/choose_5_fruit_result.txt
+echo "The fruits of the day are:" >> ~/choose_5_fruit_result.txt
+FRUIT=`cat ~/LinuxShellScript/mydata/all_fruits.txt | shuf | head -5`
+for myfruit in $FRUIT
+do
+    echo "$i. $myfruit" >> ~/choose_5_fruit_result.txt
+    echo "$i. $myfruit" >> ~/LinuxShellScript/linkdata/current/$HOUR/record.txt
+
+    ((i++))
+done
+echo "-----------------------------------------------------" >> ~/choose_5_fruit_result.txt
+echo "-----------------------------------------------------" >> ~/LinuxShellScript/linkdata/current/$HOUR/record.txt
